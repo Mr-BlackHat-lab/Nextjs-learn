049399c620a1876c9d1dedb35fbd94f83ec30725
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-08-08 10:56:37 +0530

    wireframe in postpage

--- src/app/projects/posts/page.js
+++ src/app/projects/posts/page.js
@@ -2,19 +2,21 @@
 'use client';
 import { useEffect, useState } from 'react';
 import '@/styles/posts.css';
+import { setConfig } from 'next/config';
 
 export default function PostsPage() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(null);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
       .then(res => res.json())
-      .then(data => setPosts(data));
+      .then(data => setPosts(data))
   }, []);
 
   return (
     <div className="posts-page">
       <h1 className="posts-title">Posts</h1>
+      {posts ?
       <ul className="posts-list">
         {posts.map(post => (
           <li key={post.id} className="post-item">
@@ -23,6 +25,14 @@ export default function PostsPage() {
           </li>
         ))}
       </ul>
+      :Array(5).fill(0).map((_,i) =>(
+        <div key={i} className='skeleton'>
+          <div className='skeleton-item'>
+            <div className='skeleton-titel'></div>
+            <div className='skeleton-body'></div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
